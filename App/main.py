c20738253b420e0663f50b629a9f59a1a97acf6a
Philip Wilson <jpw1n17@soton.ac.uk>
2017-12-19 17:44:51 +0000

    update plots

--- App/main.py
+++ App/main.py
@@ -1,10 +1,14 @@
-from flask import Flask, flash, render_template,jsonify, request, redirect, url_for, session
+from flask import Flask, flash, render_template,jsonify, request, redirect, url_for, session, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
