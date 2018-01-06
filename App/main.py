aaf3770b088a546782fd1caefbc3c34b104d4cfe
Philip Wilson <jpw1n17@soton.ac.uk>
2018-01-06 22:19:39 +0000

    added scatter plots for low freq data

--- App/main.py
+++ App/main.py
@@ -3,8 +3,13 @@ from flask import Flask, flash, render_template,jsonify, request, redirect, url_
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
+
+@app.route('/index2.html')
+def index2():
+    return render_template('index2.html')
 
 @app.route('/static/<path:path>')
 def send_js(path):
